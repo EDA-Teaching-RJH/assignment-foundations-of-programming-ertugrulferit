7e232225e866270d5ea9fdea7cc7a93ef858f7d2
ertugrulferit <ef11@kent.ac.uk>
2026-01-29 11:55:25 +0000

    B2

--- fleet_manager.py
+++ fleet_manager.py
@@ -4,3 +4,13 @@ def view_crew():
         print(n[i] + " - " + r[i])
 if opt== "1":
     view_crew()
+def add_crew():
+    new_name = input("Name: ")
+    new_rank = input("Rank: ")
+    new_division = input("Division: ")
+    n.append(new_name)
+    r.append(new_rank)
+    d.append(new_division)
+    print("Crew member added.")
+if opt == "2":
+    add_crew()
