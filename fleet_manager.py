a9349ffe688f5b943e0db36d5fc47ffff0e78ff5
ertugrulferit <ef11@kent.ac.uk>
2026-01-31 18:17:57 +0000

    B5

--- fleet_manager.py
+++ fleet_manager.py
@@ -5,36 +5,81 @@ def init_database():
     i = ["101", "102", "103", "104", "105"]
     return n, r, d, i
 
-def view_crew(n, r):
-    print("crew list:")
-    for i in range(len(n)):
-        print(n[i] + " - " + r[i])
+def view_crew(names, ranks, ids):
+    print("\n--- Current Crew List ---")
+    for index in range(len(names)):
+        print("ID: " + str(ids[index]) + " | " + names[index] + " - " + ranks[index])
 
-def add_crew(n, r, d):
-    new_name = input("Name: ")
-    new_rank = input("Rank: ")
-    new_division = input("Division: ")
-    n.append(new_name)
-    r.append(new_rank)
-    d.append(new_division)
+def add_crew(names, ranks, divs, ids):
+    new_id = input("New ID: ")
+    if new_id in ids:
+        print("Error: ID already exists!")
+        return
+    names.append(input("Name: "))
+    ranks.append(input("Rank: "))
+    divs.append(input("Division: "))
+    ids.append(new_id)
     print("Crew member added.")
+
+def remove_crew(names, ranks, divs, ids):
+    rem_id = input("Enter ID to remove: ")
+    if rem_id in ids:
+        idx = ids.index(rem_id)
+        names.pop(idx)
+        ranks.pop(idx)
+        divs.pop(idx)
+        ids.pop(idx)
+        print("Crew member removed.")
+    else:
+        print("ID not found.")
+
+def calculate_payroll(ranks):
+    total_pay = 0
+    for rank in ranks:
+        if rank == "Captain":
+            total_pay += 100001
+        elif rank == "Commander":
+            total_pay += 80000
+        elif rank == "Lieutenant":
+            total_pay += 70000
+        elif rank == "Engineer":
+            total_pay += 99999
+        elif rank == "Communications Officer":
+            total_pay += 60000
+        else:
+            total_pay += 500
+    return total_pay
+
+def search_crew(names, ids):
+    searchid = input("Enter ID to search: ")
+    if searchid in ids:
+        idx = ids.i(searchid) 
+        print("Found: " + names[idx])
+    else:
+        print("Crew member not found.")
 
 def main():
     n, r, d, i = init_database()
+    name = input("Login Name: ")
     
     while True:
-        print("\n1. View Crew")
-        print("2. Add Crew")
-        print("3. Exit")
-        
-        opt = input("Select option: ")
+        print("\nTerminal Active: " + name)
+        print("1. View | 2. Add | 3. Remove | 4. Payroll | 5. Search | 6. Exit")
+        opt = input("Choice: ")
         
         if opt == "1":
-            view_crew(n, r)
+            view_crew(n, r, i)
         elif opt == "2":
-            add_crew(n, r, d)
+            add_crew(n, r, d, i)
         elif opt == "3":
-            print("Shutting down.")
+            remove_crew(n, r, d, i)
+        elif opt == "4":
+            total = calculate_payroll(r)
+            print("Total Payroll: $" + str(total))
+        elif opt == "5":
+            search_crew(n, i)
+        elif opt == "6":
+            print("Shutting down...")
             break
 
 main()
